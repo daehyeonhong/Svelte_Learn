6be1accf036082e28254764533dd80c4a79a183c
HonGDae <fholng@outlook.com>
2022-02-02 03:21:18 +0900

    Event Handling V2

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,22 @@
 <script>
-    import {onMount} from 'svelte';
-
     let name = 'World';
     let isRed = false;
-    onMount(() => {
-        const box = document.querySelector('.box');
-        box.addEventListener('click', () => isRed = !isRed);
-    });
+
+    function enter() {
+        name = 'enter';
+    }
+
+    function leave() {
+        name = 'leave';
+    }
 </script>
 <h1>Hello, {name}!</h1>
-<div class="box" style="background-color: {isRed?'red':'orange'}">Box!</div>
+<div class="box" style="background-color: {isRed?'red':'orange'}"
+     on:click={()=>isRed=!isRed}
+     on:mouseenter={enter}
+     on:mouseleave={leave}
+>Box!
+</div>
 <style>
     .box {
         width: 300px;
